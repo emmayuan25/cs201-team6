8ac287ac1d8d235ec09c5d84e3613b3139b28ed8
Adam Lehavi <alehavi@usc.edu>
2022-11-14 16:51:50 -0800

    Added search functionality

---
+++ trojanconnect/src/components/SearchResult.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function SearchResult(props) {
+    const bufferStyle = {
+        backgroundColor: '#990000',
+        height: '40px',
+    }
+
+    const searchBarStyle = {
+        height: '100px',
+        width: '100%',
+        backgroundColor: '#ffcc00'
+    }
+
+    const h1Style = {
+        fontSize: "70px",
+        textAlign: "center",
+        color: "white"
+      };
+
+    const dropDownStyle = {
+        height: '70px',
+        backgroundColor: 'white',
+
+    }
+
+    const dropTextStyle = {
+        fontSize: '30px',
+        fontWeight: 'bold',
+        lineHeight: '70px',
+        paddingLeft: '60px'
+    }
+
+    const userStyle = {
+        height: '30px',
+        float: 'left',
+        margin: '20px'
+    }
+
+    const searchStyle = {
+        height: '70px',
+        float: 'left',
+        marginTop: '15px',
+        marginLeft: '30px'
+    }
+
+    const profile = props.profile
+
+    return (
+            <div style={dropDownStyle}>
+                <img src={profile} style = {userStyle} alt='User'></img>
+                <p style={dropTextStyle}>Person 1</p>
+            </div>
+    );
+
+}
+
+export default SearchResult;
--- trojanconnect/src/pages/SearchPage.js
+++ trojanconnect/src/pages/SearchPage.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
 import SearchPageBorder from '../components/SearchPageBorder';
 import Footer from '../components/Footer';
+import SearchResult from '../components/SearchResult';
+
+const mockUsers = [{name: 'Adam', uid: 1, profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1600px-Image_created_with_a_mobile_phone.png'},
+    {name: 'Emma', uid: 2, profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1600px-Image_created_with_a_mobile_phone.png'},
+    {name: 'Jacky', uid: 3, profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1600px-Image_created_with_a_mobile_phone.png'},
+    {name: 'John', uid: 4, profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1600px-Image_created_with_a_mobile_phone.png'}]
+
 
 const SearchPage = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [results, setResults] = useState([]);
+    const [search, setSearch] = useState("");
 
+    const getUsers = (term) => {
+        console.log(term);
+        var users = []
+        // replace with query
+        for (let user of mockUsers)
+            if(user.name.toLowerCase().includes(term.toLowerCase())){
+                users.push(user)
+            }
+        setResults(users)
+    }
+    
+    // useEffect(() => {
+    //     setIsLoaded(true);        
+    // }, [results]);
     return (
         <>
             <div className='flex flex-row'>
                 <SearchPageBorder></SearchPageBorder>
-                
+                <form onSubmit={(e) => {
+                    console.log("SUBMITTED")
+                    e.preventDefault();
+                    getUsers(search);
+                    setSearch("")
+                }}>
+                    <input type='text' placeholder='Search' value={search}
+                    onChange={(e) => setSearch(e.target.value)}/>
+                    <input type='submit' value='Submit'/>
+                </form>
                 <SearchBar></SearchBar>
-                
+                {results.map((user) => (
+                    <div key={user.uid} onClick={() => {
+                        console.log(user.uid);
+                    }}>
+                        <img src={user.profile}/>
+                        <div> {user.name}</div>
+                    <SearchResult profile={user.profile}/>
+                    </div>
+                ))}
                 <SearchPageBorder></SearchPageBorder>
             </div>
             <Footer />
