d9ad6eab4ad01c53769ff08ab31e92024c296a39
Komei Ryu <eryu@usc.edu>
2022-11-19 11:22:59 -0800

    Update from Eric

---
+++ trojanconnect/ChatMessage.java
@@ -0,0 +1,80 @@
+package eryu_CSCI201_GroupProject;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+
+public class ChatMessage {
+	private int messageID;
+	private int senderID;
+	private int receiverID;
+	private String senderUsername;
+	private String receiverUsername;
+	private String senderProfilePicture;
+	private String receiverProfilePicture;
+	private String messageText;
+	private Timestamp timestamp;
+	private SimpleDateFormat timeFormatter;
+	
+	public ChatMessage(int messageID, int senderID, int receiverID, String senderUsername, String receiverUsername, String senderProfilePicture, String receiverProfilePicture, String messageText, Timestamp timestamp) {
+		this.messageID = messageID;
+		this.senderID = senderID;
+		this.receiverID = receiverID;
+		this.senderUsername = senderUsername;
+		this.receiverUsername = receiverUsername;
+		this.senderProfilePicture = senderProfilePicture;
+		this.receiverProfilePicture = receiverProfilePicture;
+		this.messageText = messageText;
+		this.timestamp = timestamp;
+		
+		// reference for SimpleDateFormat: https://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html
+		// and reference: https://www.geeksforgeeks.org/program-to-convert-milliseconds-to-a-date-format-in-java/
+		// and reference: https://www.javatpoint.com/java-get-current-date
+		String timePattern = "yyyy-MM-dd HH:mm:ss";
+		this.timeFormatter = new SimpleDateFormat(timePattern);
+	}
+	public int getMessageID() {
+		return this.messageID;
+	}
+	public int getSenderID() {
+		return this.senderID;
+	}
+	public int getReceiverID() {
+		return this.receiverID;
+	}
+	public String getSenderUsername() {
+		return this.senderUsername;
+	}
+	public String getReceiverUsername() {
+		return this.receiverUsername;
+	}
+	public String getSenderProfilePicture() {
+		return this.senderProfilePicture;
+	}
+	public String getReceiverProfilePicture() {
+		return this.receiverProfilePicture;
+	}
+	public String getMessageText() {
+		return this.messageText;
+	}
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+	public String getTimestampString() {
+		String timestampString = this.timeFormatter.format(this.timestamp);
+		return timestampString;
+	}
+}
+
+//reference: https://www.geeksforgeeks.org/comparator-interface-java/
+class ChatMessageComparator implements Comparator<ChatMessage> {
+	public int compare(ChatMessage a, ChatMessage b) {
+		Timestamp a_messageTime = a.getTimestamp();
+		Timestamp b_messageTime = b.getTimestamp();
+		
+		// if a has a later messageTime, it should come first when sorting
+		if(a_messageTime.compareTo(b_messageTime) > 0) return -1;
+		else if(a_messageTime.compareTo(b_messageTime) < 0) return 1;
+		else return 0;
+	}
+}
---
+++ trojanconnect/Post.java
@@ -0,0 +1,59 @@
+package eryu_CSCI201_GroupProject;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class Post {
+	private int postID;
+	private String postText;
+	private String postImage;
+	private Timestamp timestamp;
+	private String username;
+	private String profilePicture;
+	private boolean edited;
+	private SimpleDateFormat timeFormatter;
+	
+	public Post(int postID, String postText, String postImage, Timestamp timestamp, String username, String profilePicture, boolean edited) {
+		this.postID = postID;
+		this.postText = postText;
+		this.postImage = postImage;
+		this.timestamp = timestamp;
+		this.username = username;
+		this.profilePicture = profilePicture;
+		this.edited = edited;
+		
+		// reference for SimpleDateFormat: https://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html
+		// and reference: https://www.geeksforgeeks.org/program-to-convert-milliseconds-to-a-date-format-in-java/
+		// and reference: https://www.javatpoint.com/java-get-current-date
+		String timePattern = "yyyy-MM-dd HH:mm:ss";
+		this.timeFormatter = new SimpleDateFormat(timePattern);
+	}
+	public int getPostID() {
+		return this.postID;
+	}
+	public String getPostText() {
+		return this.postText;
+	}
+	public String getPostImage() {
+		return this.postImage;
+	}
+	public String getUsername() {
+		return this.username;
+	}
+	public String getProfilePicture() {
+		return this.profilePicture;
+	}
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+	public String getTimestampString() {
+		String timestampString = this.timeFormatter.format(this.timestamp);
+		return timestampString;
+	}
+	public boolean getEdited() {
+		return this.edited;
+	}
+	public void setEdited(boolean edited) {
+		this.edited = edited;
+	}
+}
