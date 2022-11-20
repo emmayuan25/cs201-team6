ccc8b399f7beee665eb9b2633eba1cc63c98518a
Pia Rodriguez <pvrodrig@usc.edu>
2022-11-20 10:21:49 -0800

    Update ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -54,21 +54,19 @@ public class ServerThread extends Thread{
 			int id= user.getUserID();
 			return id;
 		}
+	    
+	    public String getUserName() {
+			String username= user.getUsername();
+			return username;
+		}
 
 		//send messages to user
 	    public void sendMessage(String message){
 	        try{
-	            StringTokenizer st = new StringTokenizer(message, "#");
-	            String MsgToSend = st.nextToken();
-	            int recipient = Integer.parseInt(st.nextToken());
-	            for(ServerThread x: sr.getVectorThread()) {
-	            	if(x.getUserID()==recipient) {
-	            		x.output.writeUTF(this.user.getUsername() +" : "+ MsgToSend);
-	            		break;
-	            	}
-	            }
-//	        	output.writeObject(message);
-//	            output.flush();
+	        	output.writeObject(message);
+	            output.flush();
+
+
 	        }catch (IOException e){
 	            System.out.println("ioe in severthread sendMessage(): "+ e.getMessage());
 	        }
@@ -81,67 +79,155 @@ public class ServerThread extends Thread{
 	    	//Prompt users to either log in or sign up
 	        
 			try {
-				sendMessage("Please log in or sign up");
-		        String response;
-				response = br.readLine();
-				//if selected log in, call userAuthentication()
+				//connect
 				
-		        if(response.equals("log in")) {
-		        	 sendMessage("Username:");
-		        	 String username = br.readLine();
-		        	 sendMessage("Password:");
-		        	 String password = br.readLine();
-		        	 user = sr.userAuthentication(username, password);
-		        }
-		        //if selected sign up, call create new user
-		        else if(response.equals("sign up")) {
-		        	user = sr.createNewUser();
-		        }
-		        
+			
+				//if selected log in, call userAuthentication()
+				while(true) {
+					sendMessage("Please log in or sign up");
+			       
+					
+					String response = (String) input.readObject();
+					System.out.println(response);
+					
+			        
+					//if user chose to log in
+					if(response.equals("log in")) {
+			        	 
+			        	sendMessage("Username:");
+			        	 String username = (String) input.readObject();
+			        	 
+			        	 sendMessage("Password:");
+			        	 String password = (String) input.readObject();
+			        	
+			        	 user = sr.userAuthentication(username, password);
+			        	 if(user!=null) {
+			        		 
+			        		 break;
+			        		 
+			        	 }else {
+			        		 sendMessage("Invalid username or password please try again.");
+			        	 }
+			        }
+			        
+					//if selected sign up, call create new user
+			        else if(response.equals("sign up")) {
+			        	System.out.println("Creating new user");
+			        
+			        	//get username
+			        	sendMessage("Username: ");
+			        	String userName=(String) input.readObject();
+			        	 
+			        	//get password
+			        	 sendMessage("Password: ");
+			        	String password=(String) input.readObject();
+			        	
+			        	//get photo
+			        	sendMessage("Link to profile photo: ");
+			        	String userImage= (String) input.readObject();;
+			        	
+			        	sendMessage("Interest: ");
+			        	String interest = (String) input.readObject();
+			        	String[] arrOfInt= interest.split(" ");
+			        	ArrayList<String> interestList = new ArrayList<String>();
+			        	for(String i: arrOfInt) {
+			        		System.out.println(i);
+			        		interestList.add(i);
+			        	}
+			        	
+			        	
+			        	user = sr.createNewUser(userName, password, userImage, interestList);
+			        	if(user!=null) {
+			        		 break;
+			        		 
+			        	 }	
+			        } 
+					
+				}
+				
 		        while(true) {
-		        	//displaay post while on home page
-		        	ArrayList<Post> postings = user.displayPosts();
+		        	sendMessage("What would you like to do?");
+		        	String action = (String) input.readObject();
 		        	
-		        	//for chatting -> getting messages
-		        	String received =input.readUTF();
-		        	if(received!=null) {
-		        		StringTokenizer st = new StringTokenizer(received, "#");
-			        	
-			        	String msg = st.nextToken();
-			        	String sender = st.nextToken();
-			        	
-			        	//print msg;
+		        	//display post while on home page
+		        	if(action.equals("get post")) {
+		        		ArrayList<Post> postings = user.displayPosts();
 		        	}
 		        	
+		        	
+		        	//for chatting -> getting messages
+		        	if(action.equals("chat")) {
+		        		sendMessage("What is the person you would like to send message?");
+		        		String reciever = (String) input.readObject();
+		        		for(ServerThread thread: sr.getVectorThread()) {
+		        			
+		        			
+		        			if(thread.getUserName().equals(reciever)) {
+		        				System.out.print("connection found");
+		        				sendMessage("What is the message? ");
+		        				String message = (String) input.readObject();
+		        				thread.sendMessage(user.getUsername()+": "+ message);
+		        				sr.newMessage(user.getUserID(), thread.getUserID(), message);
+		        				//TODO load chats
+		        			}
+		        		}
+		        	}
+
+		        	
 		        	//search while on search page
-		        	String search = null; // get the search term and input into class for list of results
-		        	List<String> interestList =sr.searchKeyword(search);
+		        	if(action.equals("search")) {
+		        		sendMessage("Enter search term");
+		        		
+		        		String search =(String) input.readObject(); // get the search term and input into class for list of results
+			        	List<String> interestList =sr.searchKeyword(search, user.getUserID());
+		        	}
 		        	
-		        	//delete user
-		        	sr.deleteUser(user);
-		        	
-		        	
-		        	//create post
-		        	String postText = null;
-		        	String postImage = null;
-		        	String postInterest = null;
-		        	Post post = user.createNewPost(postText, postImage, postInterest);
-		        	
-		        	
-		        	//edit existing post
-		        	int postID = 0;
-		        	String newPostText = null;
-		        	String newPostImage = null;
-		        	user.editExistingPost(postID, newPostText,newPostImage);
+		        	//Delete
+		        	if(action.equals("delete")) {
+		        		sr.deleteUser(user);
+		        	}
 		        	
 		        	
-		        	//get chats
-		        	ArrayList<Chat> chat = user.getCurrentChats();
+		        	
+		        	
+		        	//TODO create post
+		        	if(action.equals("create")) {
+		        		String postText = null;
+			        	String postImage = null;
+			        	String postInterest = null;
+			        	Post post = user.createNewPost(postText, postImage, postInterest);
+			        	
+		        	}
+		        	
+		        	//TODO edit existing post
+		        	if(action.equals("edit")) {
+			        	int postID = 0;
+			        	String newPostText = null;
+			        	String newPostImage = null;
+			        	user.editExistingPost(postID, newPostText,newPostImage);
+		        	}
+		        	
+		        	//TODO delete post
+		        	if(action.equals("delete post")) {
+		        		int postID=0;
+		        		user.deletePost(postID);
+		        	}
+		        	
+		        	//TODO get chat and display
+		        	if(action.equals("get chat")) {
+		        		//get chats
+			        	ArrayList<Chat> chat = user.getCurrentChats();
+			        	
+		        	}
+		        	
+		     
+		        	
+		        	
 		        	
 		        	
 
 		        }
-			} catch (IOException e) {
+			} catch (IOException | ClassNotFoundException e) {
 				System.out.print("IO exception in serverThread run() " + e );
 			}
 	        
@@ -151,6 +237,8 @@ public class ServerThread extends Thread{
 	    
 	    
 }
+
+
 
 
 
