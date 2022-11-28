95bee748ff3d2c1fd4b9158fb6afdd96be71578b
Jacky Au-Yeung <jackyauy@usc.edu>
2022-11-28 03:09:20 -0800

    Guest Page Updated

--- trojanconnect/src/pages/GuestPage.js
+++ trojanconnect/src/pages/GuestPage.js
@@ -9,11 +9,25 @@ function GuestPage(props) {
     const [isLoggedin, setIsLoggedin] = [props.isLoggedin, props.setIsLoggedin];
     const navigate = useNavigate();
 
+    const logout = props.logout
+    const signUp = props.signUp
+    const guest = props.guest
+    const signIn = props.signIn
+
 return (
 	<>
 	<div className= "newBackgroundColor">
 	<div style={{ textAlign: 'center', color: 'white' }}> 
     <h1>You are a guest, please login or sign up to view this functionality.</h1>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <button type="Sign In" onClick={signIn} style = {{width: "175px", height: "35px"}}>
+		<a href="/"> Back to Login Page </a>
+	</button>
+    <br></br>
+	<br></br>
+	<button type="Sign Up" onClick={signUp} style = {{width: "175px", height: "35px"}}>
+		<a href="/SignUp"> Create Account </a>
+	</button>
 	</div>
 	</div>
 	</>
