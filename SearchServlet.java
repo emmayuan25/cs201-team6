6928ef9901776424d1eaa91db5f9af5d94ba6f7b
Pia Rodriguez <pvrodrig@usc.edu>
2022-11-27 17:47:05 -0800

    Update SearchServlet.java

--- SearchServlet.java
+++ SearchServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,15 +25,22 @@ public class SearchServlet extends HttpServlet{
 			
 			HttpSession session = request.getSession(false);
 			User user = (User) session.getAttribute("user");
+			System.out.println("user id: "+user.getUserID());
+			//TODO might have to change it so it can be a list of User, so there is the functionality to open user profile and message them
 			List<String> searchList = JDBCConnector.searchKeyword(search, user.getUserID());
+
 			
-			//TODO idk what to print or do after getting list
-			out.println(searchList);
+			//TODO send out postList to js
+			//this will send the list of post objects to the js function calling this class
+			//TODO change "list" name
+			request.setAttribute("list", searchList);
+			//TODO add JSP file name
+			request.getRequestDispatcher("JSP FILE NAME").forward(request, response);
 			
 			
 			
-		} catch (IOException e) {
-			System.out.println("IOException: "+ e);
+		} catch (IOException | ServletException e) {
+			System.out.println("IOException in searchServlet: "+ e);
 		}
 		
 		
