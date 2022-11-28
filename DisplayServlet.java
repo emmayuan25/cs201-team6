ce84fb0ab268235a35deb60c8f7df4f09eae0033
Pia Rodriguez <pvrodrig@usc.edu>
2022-11-27 17:31:00 -0800

    Update DisplayServlet.java

--- DisplayServlet.java
+++ DisplayServlet.java
@@ -2,7 +2,9 @@ package team;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +22,22 @@ public class DisplayServlet extends HttpServlet{
 		
 			HttpSession session = request.getSession(false);
 			User user = (User) session.getAttribute("user");
+
+			List<Post> postList = JDBCConnector.displayPosts(user);
+			
+			//TODO send out postList to js
+			//this will send the list of post objects to the js function calling this class
+			//TODO change "list" name
+			request.setAttribute("list", postList);
+			//TODO add JSP file name
+			request.getRequestDispatcher("JSP FILE NAME").forward(request, response);
 			
 			
 			
 		} catch (IOException e) {
 			System.out.println("IOException in DisplayServlet: "+ e);
+		} catch (ServletException e) {
+			System.out.println("Server Exception in DisplayServlet: "+ e);
 		}
 		
 		
