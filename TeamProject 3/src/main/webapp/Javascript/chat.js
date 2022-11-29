c8b17a536d6dbbde90c66ed55300c8dd9809e8c9
Emma Yuan <jinyueyu@usc.edu>
2022-11-29 14:41:01 -0800

    chat page frontend done

---
+++ TeamProject 3/src/main/webapp/Javascript/chat.js
@@ -0,0 +1,49 @@
+console.log("here");
+
+var contactList = document.getElementById("chat-list");
+
+console.log(contactList);
+
+function addContact(name, userid) {
+    var newContact = document.createElement('div');
+    var content = `
+        <div class='contact-box' onclick="selectChat(this, `+ userid +`);">
+            <img class='contact-icon' src='./assets/user.png' alt='user' />
+            <span class='contact-name' >` + name + `</span></div>`;
+    newContact.innerHTML = content;
+    return newContact;
+}
+
+contactList.appendChild(addContact('user1', 1));contactList.appendChild(addContact('user2', 2));contactList.appendChild(addContact('user3', 3));
+
+// select user function
+
+
+// display message
+var chatbox = document.getElementById("chat-box");
+function addMsg(text, out) {
+    var newMsg = document.createElement('div');
+    if(out == true) {
+        var content =  `
+            <div class="msgboxl">
+                <div class="msgtext">`
+                + text + `</div>
+                <img class='msguser' src="./assets/user.png" alt='user' /></div>`;
+        newMsg.innerHTML = content;
+        
+    } else {
+        var content =  `
+            <div class="msgboxr">
+                <img class='msguser' src='./assets/user.png' alt='user' />
+                <div class="msgtext">`
+                + text + `</div></div>`;
+        newMsg.innerHTML = content;
+    }
+
+    return newMsg;
+}
+
+
+chatbox.appendChild(addMsg("test message out", true));
+chatbox.appendChild(addMsg("test message in", false));
+chatbox.appendChild(addMsg("test message out", true));
